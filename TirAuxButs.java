60849f757ed1592999837433cddea2b39a873334
AboubacarDby <147718557+AboubacarDby@users.noreply.github.com>
2023-12-04 10:25:08 +0100

    Update TirAuxButs.java

--- TirAuxButs.java
+++ TirAuxButs.java
@@ -30,12 +30,12 @@ public class TirAuxButs {
 	    jetIa = d20();
 	    choixIa = verticale();
 	    choixIa2 = horizontale();
-	    // Tour du joueur
+	    // Joueur tire
 	    System.out.println("Où voulez-vous tirer :");
 	    System.out.print("\t1 - en haut à gauche \t2 - en haut au mileu \t3 - en haut à droite");
 	    System.out.println("\t4 - au milieu à gauche \t5 - au milieu au centre \t6 - au milieu à droite");
 	    System.out.println("\t7 - en bas à gauche \t8 - au bas au milieu \t9 - en bas à droite");
-	    // saisie du choix
+	    // l'utilisateur saisie son choix
 	    choixJoueur = saisie.nextInt();
 
 	    // choix de zone du tireur en verticale
@@ -54,12 +54,12 @@ public class TirAuxButs {
 	    } else {
 		horizontale = 3;
 	    }
-	    System.out.println(choixIa + " et " + choixIa2);
+//	    System.out.println(choixIa + " et " + choixIa2);
 	    if (verticale - choixIa2 == -1 || verticale - choixIa2 == 1) {
 		jetIa = jetIa - 3;
 	    } else if (horizontale - choixIa == 1 || horizontale - choixIa == -1) {
 		jetIa = jetIa - 5;
-		// faire une opération pour horizontale et verticale séparées
+
 	    } else if (horizontale - choixIa == 2) {
 		jetIa = jetIa - 10;
 	    } else if (verticale - choixIa2 == -2) {
@@ -72,20 +72,20 @@ public class TirAuxButs {
 		System.out.println("le gardien reste immobile !");
 	    } else if (jetJoueur >= jetIa) {
 		System.out.println("But du tireur !");
-		scoreEquipeJoueur = +1;
+		scoreEquipeJoueur = scoreEquipeJoueur + 1;
 	    } else {
 		System.out.println("Quelle arrêt du gardien !");
 	    }
 
 	    System.out.println(scoreEquipeJoueur + " à " + scoreEquipeIa);
 
-	    // Tour de l'IA
+	    // IA tire
 	    System.out.println("Où voulez-vous plonger :");
 	    System.out.print("\t1 - en haut à gauche \t2 - en haut au mileu \t3 - en haut à droite");
 	    System.out.println("\t4 - au milieu à gauche \t5 - au milieu au centre \t6 - au milieu à droite");
 	    System.out.println("\t7 - en bas à gauche \t8 - au bas au milieu \t9 - en bas à droite");
 	    choixJoueur = saisie.nextInt();
-	    // choix du joueur en verticale
+	    // choix du gardien en verticale
 	    if (choixJoueur == 1 || choixJoueur == 2 || choixJoueur == 3) {
 		verticale = 1;
 	    } else if (choixJoueur == 4 || choixJoueur == 5 || choixJoueur == 6) {
@@ -93,7 +93,7 @@ public class TirAuxButs {
 	    } else {
 		verticale = 3;
 	    }
-	    // choix de zone du tireur en horizontale
+	    // choix du gardien en horizontale
 	    if (choixJoueur == 1 || choixJoueur == 4 || choixJoueur == 7) {
 		horizontale = 1;
 	    } else if (choixJoueur == 2 || choixJoueur == 5 || choixJoueur == 8) {
@@ -101,7 +101,7 @@ public class TirAuxButs {
 	    } else {
 		horizontale = 3;
 	    }
-	    System.out.println(verticale + " et " + horizontale);
+//	    System.out.println(verticale + " et " + horizontale);
 	    if (verticale - choixIa2 == -1 || verticale - choixIa2 == 1) {
 		jetJoueur = jetJoueur - 3;
 	    } else if (horizontale - choixIa == 1 || horizontale - choixIa == -1) {
@@ -112,27 +112,32 @@ public class TirAuxButs {
 	    } else if (verticale - choixIa2 == -2) {
 		jetJoueur = jetJoueur - 10;
 	    }
-
+// comparaison
 	    if (jetIa == 1) {
 		System.out.println("Le tireur rate son geste !");
 
-	    }
-	    if (jetJoueur == 1) {
+	    } else if (jetJoueur == 1) {
 		System.out.println("le gardien reste immobile !");
-	    }
-	    if (jetIa > jetJoueur) {
-		System.out.println("But du tireur !");
-		scoreEquipeIa = +1;
-	    } else if (jetIa < jetJoueur) {
+	    } else if (jetIa >= jetJoueur) {
+		System.out.println("But du tireur adverse !");
+		scoreEquipeIa = scoreEquipeIa + 1;
+	    } else {
 		System.out.println("Quelle arrêt du gardien !");
 	    }
+
 	    System.out.println(scoreEquipeJoueur + " à " + scoreEquipeIa);
 
 	    // incrémenation du tour de jeu jusqu'à arriver à la fin
 	    tourDeJeu++;
 
 	}
-	System.out.println("Le score final est de " + scoreEquipeJoueur + " à " + scoreEquipeIa);
+	if (scoreEquipeJoueur > scoreEquipeIa) {
+	    System.out.println("Victoire de l'Equipe !");
+	} else if (scoreEquipeJoueur < scoreEquipeIa) {
+	    System.out.println("Victoire de l'ordinateur !");
+	} else {
+	    System.out.println("C'est un match nul !");
+	}
     }
 
 // méthode pour le jet de dé pour le tireur et le gardien
@@ -161,3 +166,4 @@ public class TirAuxButs {
     }
 
 }
+
